96eb2d4940f70328f5e17f0866561e0363d834da
edjunma <edjunma@gmail.com>
2021-02-17 15:59:17 -0500

    still working on scroll reveal, work on left/right ani

--- main.js
+++ main.js
@@ -13,6 +13,16 @@ menuToggler.addEventListener('click', function () {
 // Scroll reveal
 window.sr = ScrollReveal();
 
-sr.reveal() {
-  
-}
+sr.reveal('.animate-left', {
+  origin: 'left',
+  duration: 1000,
+  distance: '25rem',
+  delay: 300
+});
+
+sr.reveal('.animate-right', {
+  origin: 'right',
+  duration: 1000,
+  distance: '25rem',
+  delay: 600
+});
